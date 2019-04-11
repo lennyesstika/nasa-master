e4677b57f13e354512eb2a3a9a3f20f34503d9b7
lennyesstika <43899877+lennyesstika@users.noreply.github.com>
2019-04-11 18:34:04 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=kC80fUg8DYDtpn2uz5NML1L9qcomOBGE6EPY8PTA";
         
         $("#loadImage").click(function(){
             $.ajax({
